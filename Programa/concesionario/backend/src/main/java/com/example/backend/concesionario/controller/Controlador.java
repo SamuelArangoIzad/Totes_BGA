6bf406026f21d2ea5f291d6eb19c644b53d4ea77
SamuelArangoIzad <samuelarangodiaz04@gmail.com>
2025-02-19 15:54:48 -0500

    Update

---
+++ Programa/concesionario/backend/src/main/java/com/example/backend/concesionario/controller/Controlador.java
@@ -0,0 +1,15 @@
+package com.example.backend.concesionario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controlador {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("mensaje", "");
+        return "index"; // Busca templates/index.html
+    }
+}
